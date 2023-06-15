63eb3e50695ddf152b12ea8165b424620f8fe512
ZoherAli <apidon888@gmail.com>
2023-06-15 01:02:59 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints a line with put function 
+ * Return: 0(Sucsess)
+ */
+int main(void){ puts("\"Programming is like building a multilingual puzzle");
+	return (0);}
