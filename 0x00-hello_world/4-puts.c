8eac94a45afa30288a2ca08385400d11035000d0
ZoherAli <apidon888@gmail.com>
2023-06-15 13:24:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-
-
-/**
- * main - Programming is like building a multilingual puzzle
- * Return: 0(Sucsess)
- */
-int main(void)
-
-{
-	uts("\"Programming is like building a multilingual puzzle");
-	eturn(0);
+#include <stdio.h>                                                                  
+/**                                                                                 
+ *  * Description: main - Prints out a string to stdout.                            
+ *   * Return: 0 if success.                                                        
+*/                                                                                  
+int main(void)                                                                      
+{                                                                                   
+printf("with proper grammar, but the outcome is a piece of art,\n");                
+return (0);
 }
