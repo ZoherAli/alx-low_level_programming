4dbc1bcef7080aab7b03e9dea03004a733095742
ZoherAli <apidon888@gmail.com>
2023-06-15 13:29:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#include <stdio.h>                                                                  
-/**                                                                                 
- *  * Description: main - Prints out a string to stdout.                            
- *   * Return: 0 if success.                                                        
-*/                                                                                  
-int main(void)                                                                      
-{                                                                                   
-printf("with proper grammar, but the outcome is a piece of art,\n");                
-return (0);
+#include <stdio.h>
+/**
+ * main - 4-puts.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
 }
+
